2c7f969f1bdb984294f7c11be55e1041dd7902b6
1goruxa <89235974301@mail.ru>
2020-12-18 09:55:04 +0700

    All done.

--- src/main/resources/db/migration/V2__globalsettings.sql
+++ src/main/resources/db/migration/V2__globalsettings.sql
@@ -1,3 +1,4 @@
+SET @@auto_increment_increment=1;
 insert into global_settings(code,name,value) values('1','MULTIUSER_MODE',true);
 insert into global_settings(code,name,value) values('1','POST_PREMODERATION',true);
 insert into global_settings(code,name,value) values('1','STATISTICS_IS_PUBLIC',true);
